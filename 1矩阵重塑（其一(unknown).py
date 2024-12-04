356a837809d902842e45d11b9f702d7c4ba805e4
cZanY714285 <firstczhuzany@163.com>
2024-12-04 20:34:20 +0800

    Add files via upload

---
+++ 1矩阵重塑（其一(unknown).py
@@ -0,0 +1,16 @@
+# input n,p = row & m,q = column
+n,m,p,q = map(int,input().split())
+arr = []
+
+# save every element in a list (called 'arr') row by row
+for i in range(n):
+    every_row = list(map(str,input().split()))
+    arr.extend(every_row)
+
+# output every element in 'arr' row by row
+for i in range(p):
+    result_row = arr[i * q:(i + 1) * q]
+    print(' '.join(map(str, result_row)))
+  #  for j in range(q-1):
+   #     print(result_row[j],end = ' ')
+   # print(result_row[q-1])
